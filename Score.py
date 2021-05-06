2eb5ee7b6371548c8258faf27570e10bb05002b3
Roshak7 <roshak7@gmail.com>
2021-05-06 19:35:08 +0300

    last

--- Score.py
+++ Score.py
@@ -1,19 +1,21 @@
 bot = int(input("Введите прибыль:"))
 usd = int(input("Введите текущий курс Доллара:"))
           
-sultan = bot * (66.7 / 100)
-malik = bot * (33.3 / 100)
+sultan = bot * (54.2 / 100)
+malik = bot * (45.8 / 100)
 sultanusd= sultan * usd
 malikusd = malik * usd
 procent_1 = sultanusd * (3.5 / 100)
 procent_2 = malikusd * (3.5 / 100)
 malik_back = malikusd - procent_2
-sultan_back = sultanusd - procent_1 
+sultan_back = sultanusd - procent_1
 
 
-print("Вы имеете:" , round(sultan),"USD")
-print("Вы имеете:", round(malik), "USD")
-print("Общий капитал Султана:", round(sultanusd))
-print("Общий капитал Малика:", round(malikusd))
-print("vosvrat",round(sultan_back),"Rub")
-print("vosvrat",round(malik_back),"Rub")
+print("Султан имеет:" , round(sultan),"USD")
+print("Малик имеет:", round(malik), "USD")
+print("Общий капитал Султана:", round(sultanusd), "Руб")
+print("Общий капитал Малика:", round(malikusd), "Руб")
+print("Возврат с процентами Султан:",round(sultan_back),"Rub")
+print("Возврат с процентами Малик:",round(malik_back),"Rub")
+print("Купили монетку за 45 USD")
+print("На 02.05.2021 имеем 6925 USD")
